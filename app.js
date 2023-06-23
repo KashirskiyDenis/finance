009c01290a36a1e8486af05c3e44400778f5b5be
KashirskiyDenis <waka_flocka@mail.ru>
2023-06-23 15:28:10 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,19 +2,22 @@
 
 const http = require('node:http');
 const fs = require('node:fs').promises;
+const url = require('node:url');
 const config = require('./config');
 const routes = require('./routes');
 
 const requestListener = function (req, res) {
-	fs.readFile(routes[req.url])
-		.then(contents => {
-			res.writeHead(200);
-			res.end(contents);
-		});	
+	let path = routes[url.parse(req.url).pathname] ?? './404.html';
+
+	fs.readFile(path)
+	.then(contents => {
+		res.writeHead(200);
+		res.end(contents);
+	});	
 };
 
 const server = http.createServer(requestListener);
 
 server.listen(config.port, config.host, () => {
 	console.log(`Server is running on http://${config.host}:${config.port}`);
-});		
+});
